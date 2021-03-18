530af3632df3e51e40f7b51666c2e8503075d3f3
shunhiguchi <shun.higuchi@outlook.com>
2021-03-17 21:14:12 -0400

    Add comments

--- greetr.js
+++ greetr.js
@@ -1,9 +1,10 @@
 (function(global, $) {
+    // Create a 'new' object
     const greetr = function(firstName, lastName, language) {
         return new greetr.init(firstName, lastName, language);
     };
 
-    // Internal attributes
+    // Properties hidden within the scope of the IIFE
     const supportedLanguages = ['en', 'es'];
     const greetings = {
         en: 'Hello',
@@ -18,22 +19,26 @@
         es: 'Inicio sesión'
     };
 
-    // Public atttributes and methods
+    // Directly accessible methods
     greetr.prototype = {
         fullName: function() {
             return `${this.firstName} ${this.lastName}`;
         },
+        // Check that the language is supported, included in supportedLanguages
         validate: function() {
             if (supportedLanguages.indexOf(this.language) === -1) {
                 throw 'Invalid language';
             }
         },
+        // Informal gretting
         greeting: function() {
             return `${greetings[this.language]} ${this.firstName}.`
         },
+        // Formal greeting
         formalGreeting: function() {
             return `${formalGreetings[this.language]}, ${this.fullName()}.`
         },
+        // Chainable methods
         greet: function(formal) {
             let message;
             if (formal) {
@@ -46,17 +51,20 @@
             }
             return this;
         },
+        // Log a person's name
         log: function() {
             if (console) {
                 console.log(`${logMessages[this.language]}: ${this.fullName()}`);
             }
             return this;
         },
+        // Set a language
         setLanguage: function(language) {
             this.language = language;
             this.validate;
             return this;
         },
+        // Inject a greeting into HTML
         HTMLGreeting: function(selector, formal) {
             if (!$) {
                 throw 'jQuery not loaded';
@@ -75,12 +83,18 @@
         }
     };
 
+    // Create an object
     greetr.init = function(firstName = '', lastName = '', language = 'en') {
         const self = this;
         self.firstName = firstName;
         self.lastName = lastName;
         self.language = language;
+        self.validate();
     }
+
     greetr.init.prototype = greetr.prototype;
+
+    // Attach greetr to the global object and provide the shorthand 'G$'
     global.greetr = global.G$ = greetr;
+    
 })(window, jQuery);
